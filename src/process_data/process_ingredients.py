434a1ed1a56d840c01f23c0487f950ac2a8de315
bhushansshah <bhushan.shah05@gmail.com>
2025-09-29 00:35:05 -0400

    cleaned ingredients tags

--- src/process_data/process_ingredients.py
+++ src/process_data/process_ingredients.py
@@ -1,63 +1,25 @@
-import re
 import json
 import argparse
-import nltk
-nltk.download("punkt_tab")
-nltk.download("maxent_ne_chunker_tab")
-nltk.download("words")
-nltk.download("averaged_perceptron_tagger_eng")
-nltk.download("stopwords")
-from nltk.corpus import stopwords
-from nltk import word_tokenize, pos_tag, ne_chunk
 
-def clean_tag(tag):
-    """Remove language prefix, numbers, and normalize text."""
-    # only keep en: tags
-    if not tag.startswith("en:"):
-        return None
-    text = tag[3:]  # remove "en:"
-    text = re.sub(r"\d+", "", text)  # remove digits
-    text = text.replace("-", " ").strip()
-    return text if text else None
 
 def extract_ingredients(tags):
-    stop_words = set(stopwords.words("english"))
-    ingredients = set()
-    counter = 0
-    for tag in tags:
-        cleaned = clean_tag(tag)
-        if not cleaned:
-            continue
+    ingredients = {}
+    for key, value in tags.items():
+        if value >= 100:  # only keep ingredients that appear at least 100 times
+            # remove "en:" prefix
+            ingredient = key.replace("en:", "")
+            ingredients[ingredient] = value
 
-        tokens = word_tokenize(cleaned)
-        tokens = [t for t in tokens if t.lower() not in stop_words]
-
-        if not tokens:
-            continue
-
-        # POS tagging
-        pos_tags = pos_tag(tokens)
-
-        # Named Entity Recognition
-        chunks = ne_chunk(pos_tags, binary=True)
-
-        # collect words labeled as NE or NOUNS
-        for word, pos in pos_tags:
-            if pos.startswith("NN"):  # noun
-                ingredients.add(word.lower())
-        
-        counter += 1
-        if counter % 1000 == 0:
-            print(f"Processed {counter} tags...")
-
-    return sorted(ingredients)
+    extracted_ingredients_dict = sorted(ingredients.items(), key=lambda item: item[1], reverse=True)
+    extracted_ingredients = [item[0] for item in extracted_ingredients_dict]
+    return extracted_ingredients
 
 def process_ingredients(input_json, output_json):
     with open(input_json, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    raw_tags = data.get("ingredients", [])
-    processed = extract_ingredients(raw_tags)
+    ingredients_tags = data.get("ingredients", [])
+    processed = extract_ingredients(ingredients_tags)
 
     result = {"ingredients": processed}
 
--- src/process_data/storing_ingredients_nutrients.py
+++ src/process_data/storing_ingredients_nutrients.py
@@ -6,17 +6,23 @@ def extract_tags(dataset_path, output_json_path):
     # Load dataset
     dataset = load_from_disk(dataset_path)
 
-    ingredients_set = set()
+    ingredients_dict = {}
     additives_set = set()
     minerals_set = set()
     nutriments_set = set()
     counter = 0
     for example in dataset:
         # --- Ingredients ---
+        # for ing in example.get("ingredients_text", []) or []:
+        #     lang = ing.split(":")[0]
+        #     if lang == "en":  # only keep English ingredients
+        #         ingredients_set.add(ing)
         for ing in example.get("ingredients_tags", []) or []:
-            lang = ing.split(":")[0]
-            if lang == "en":  # only keep English ingredients
-                ingredients_set.add(ing)
+            if ing.startswith("en:"):
+                if ingredients_dict.get(ing) is None:
+                    ingredients_dict[ing] = 1
+                else:
+                    ingredients_dict[ing] += 1
 
         # --- Additives ---
         for add in example.get("additives_tags", []) or []:
@@ -39,12 +45,18 @@ def extract_tags(dataset_path, output_json_path):
         if counter % 1000 == 0:
             print(f"Processed {counter} examples...")
 
-    # Add additives + minerals to ingredients if not already there
-    ingredients_set.update(additives_set)
-    ingredients_set.update(minerals_set)
+    #Add additives and minerals to ingredients_dict
+    for add in additives_set:
+        if ingredients_dict.get(add) is None:
+            ingredients_dict[add] = 1
 
+    for min_ in minerals_set:
+        if ingredients_dict.get(min_) is None:
+            ingredients_dict[min_] = 1
+
+    print("The length of ingredients - ", len(ingredients_dict))
     result = {
-        "ingredients": sorted(ingredients_set),
+        "ingredients": ingredients_dict,
         "additives": sorted(additives_set),
         "minerals": sorted(minerals_set),
         "nutriments": sorted(nutriments_set),
